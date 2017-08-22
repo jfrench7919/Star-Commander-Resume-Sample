59f61ef2b9289464eeeee3fd1b45b7304b578e4f
Josh French <jfrench7919@gmail.com>
2017-08-22 15:04:38 -0400

    First code base added

---
+++ StarCommander/StarShip.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarCommander
+{
+    public abstract class StarShip : IStarShip
+    {
+        public void Advance()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Attack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Retreat()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StarCommanderUnitTest/StarShipTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using StarCommander;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarCommander.Tests
+{
+    public class StarShipTests
+    {
+        [Fact()]
+        public void AdvanceTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void AttackTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void RetreatTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
