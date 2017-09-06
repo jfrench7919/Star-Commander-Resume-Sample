d147db512a0490397714750721bad122bfdd82d1
Josh French <jfrench7919@gmail.com>
2017-09-06 09:27:25 -0400

    Added a specFlow project and specFlow

---
+++ StarCommanderSpec/Battle.feature
@@ -0,0 +1,8 @@
+﻿Feature: Battle
+	I want to be told the sum of the fleets who survive
+
+@mytag
+Scenario: Exicute Battle
+	Given I have entered 6 Fleets into the BattleField
+	When I call startBattle
+	Then the result should be 1 Fleet with health greater than zero
---
+++ StarCommanderSpec/BattleSteps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using TechTalk.SpecFlow;
+using StarCommander.BattleFields;
+using StarCommander.Factories;
+using StarCommander.Types;
+using System.Collections.Generic;
+using Xunit;
+
+namespace StarCommanderSpec
+{
+    [Binding]
+    public class BattleSteps
+    {
+        public IBattleField battleField { get; set; } = BattleFieldFactory.CreateBattleField(BattleFieldType.Large); 
+
+        [Given(@"I have entered (.*) Fleets into the BattleField")]
+        public void GivenIHaveEnteredFleetsIntoTheBattleField(int numberOfFleets)
+        {
+            for (int i = 0; i < numberOfFleets; i++)
+            {
+                battleField.Fleets.Add(FleetFactory.CreateFleet(FleetConfigerationType.BalancedShips, BattleStratagyType.WeekShipsFirst));
+            }
+        }
+
+        [When(@"I call startBattle")]
+        public void WhenICallStartBattle()
+        {
+            battleField.StartBattle();
+        }
+
+        [Then(@"the result should be (.*) Fleet with health greater than zero")]
+        public void ThenTheResultShouldBeFleetWithHealthGreaterThanZero(int fleetsWithWorkingShips)
+        {
+            int survivingFleets = battleField.Fleets.Where(x => x.WorkingStarShips.Count > 0).Count();
+            Assert.Equal(fleetsWithWorkingShips, survivingFleets);
+        }
+    }
+}
